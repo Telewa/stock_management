bb1a4c21d0e1ecac31867b7db820945980a048b4
Emmanuel Telewa <emmanuelt2009@gmail.com>
2022-07-15 23:00:58 +0300

    fix: use textwrap for multiline strings

--- apps_dir/product/tests/tests.py
+++ apps_dir/product/tests/tests.py
@@ -1,3 +1,5 @@
+import textwrap
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.test.client import MULTIPART_CONTENT, encode_multipart, BOUNDARY
@@ -32,39 +34,41 @@ class ProductViewTest(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_202_ACCEPTED)
 
     def test_that_a_csv_file_can_be_imported(self):
-        csv_content = """
-country,sku,name,stock_change
-"dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
-"gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
-"ma","e920c573f128","Ramirez-Molina Granite Pizza","58"
-"ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
-"dz","cbf87a9be799","Foster-Harrell Table","47"
-"eg","cbf87a9be799","Foster-Harrell Table","35"
-"ke","cbf87a9be799","Foster-Harrell Table","5"
-"ma","cbf87a9be799","Foster-Harrell Table","56"
-"ng","cbf87a9be799","Foster-Harrell Table","29"
-"ug","cbf87a9be799","Foster-Harrell Table","53"
-"dz","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","45"
-"gh","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","83"
-"ke","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","47"
-"ng","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","76"
-"ug","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","11"
-"ci","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","51"
-"gh","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","24"
-"ma","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","89"
-"ng","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","43"
-"ci","25461d1b1653","Golden-Wright Tasty Wooden Fish","48"
-"dz","25461d1b1653","Golden-Wright Tasty Wooden Fish","21"
-"gh","25461d1b1653","Golden-Wright Tasty Wooden Fish","14"
-"eg","59648d3c5d6e","Ali, Thompson and Eaton Ball","54"
-"gh","59648d3c5d6e","Ali, Thompson and Eaton Ball","19"
-"ke","23f8f2dd3842","Black-Campbell Used Concrete Bike","64"
-"ma","23f8f2dd3842","Black-Campbell Used Concrete Bike","72"
-"ng","23f8f2dd3842","Black-Campbell Used Concrete Bike","4"
-"ug","23f8f2dd3842","Black-Campbell Used Concrete Bike","64"
-"ci","e099ceaacfbe","Gutierrez and Sons For repair Steel Car","65"
-"eg","e099ceaacfbe","Gutierrez and Sons For repair Steel Car","27"
-                """
+        csv_content = textwrap.dedent(
+            """
+                country,sku,name,stock_change
+                "dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
+                "gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
+                "ma","e920c573f128","Ramirez-Molina Granite Pizza","58"
+                "ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
+                "dz","cbf87a9be799","Foster-Harrell Table","47"
+                "eg","cbf87a9be799","Foster-Harrell Table","35"
+                "ke","cbf87a9be799","Foster-Harrell Table","5"
+                "ma","cbf87a9be799","Foster-Harrell Table","56"
+                "ng","cbf87a9be799","Foster-Harrell Table","29"
+                "ug","cbf87a9be799","Foster-Harrell Table","53"
+                "dz","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","45"
+                "gh","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","83"
+                "ke","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","47"
+                "ng","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","76"
+                "ug","fb4d42219274","Christian, Santos and Campbell Refined Frozen Car","11"
+                "ci","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","51"
+                "gh","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","24"
+                "ma","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","89"
+                "ng","041f4e51a824","Lowe, Higgins and Cooley Ergonomic Wooden Pizza","43"
+                "ci","25461d1b1653","Golden-Wright Tasty Wooden Fish","48"
+                "dz","25461d1b1653","Golden-Wright Tasty Wooden Fish","21"
+                "gh","25461d1b1653","Golden-Wright Tasty Wooden Fish","14"
+                "eg","59648d3c5d6e","Ali, Thompson and Eaton Ball","54"
+                "gh","59648d3c5d6e","Ali, Thompson and Eaton Ball","19"
+                "ke","23f8f2dd3842","Black-Campbell Used Concrete Bike","64"
+                "ma","23f8f2dd3842","Black-Campbell Used Concrete Bike","72"
+                "ng","23f8f2dd3842","Black-Campbell Used Concrete Bike","4"
+                "ug","23f8f2dd3842","Black-Campbell Used Concrete Bike","64"
+                "ci","e099ceaacfbe","Gutierrez and Sons For repair Steel Car","65"
+                "eg","e099ceaacfbe","Gutierrez and Sons For repair Steel Car","27"
+            """
+        )
 
         # given that we have no products or stock set up
         self.assertEqual(Product.objects.count(), 0)
@@ -82,13 +86,15 @@ country,sku,name,stock_change
 
     def test_get_product_by_id(self):
         # Given that we have these items in store
-        csv_content = """
-country,sku,name,stock_change
-"dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
-"gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
-"ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
-"ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
-        """
+        csv_content = textwrap.dedent(
+            """
+                country,sku,name,stock_change
+                "dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
+                "gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
+                "ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
+                "ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
+            """
+        )
         # When we upload a CSV file
         self.upload_csv(csv_content)
 
@@ -100,18 +106,20 @@ country,sku,name,stock_change
 
     def test_that_stock_can_be_consumed(self):
         # Given that we have some stock
-        csv_content = """
-country,sku,name,stock_change
-"dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
-"gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
-"ma","e920c573f128","Ramirez-Molina Granite Pizza","58"
-"ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
-"dz","cbf87a9be799","Foster-Harrell Table","47"
-"eg","cbf87a9be799","Foster-Harrell Table","35"
-"ke","cbf87a9be799","Foster-Harrell Table","5"
-"ma","cbf87a9be799","Foster-Harrell Table","56"
-"ng","cbf87a9be799","Foster-Harrell Table","29"
-                """
+        csv_content = textwrap.dedent(
+            """
+                country,sku,name,stock_change
+                "dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
+                "gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
+                "ma","e920c573f128","Ramirez-Molina Granite Pizza","58"
+                "ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
+                "dz","cbf87a9be799","Foster-Harrell Table","47"
+                "eg","cbf87a9be799","Foster-Harrell Table","35"
+                "ke","cbf87a9be799","Foster-Harrell Table","5"
+                "ma","cbf87a9be799","Foster-Harrell Table","56"
+                "ng","cbf87a9be799","Foster-Harrell Table","29"
+            """
+        )
         self.upload_csv(csv_content)
         self.assertEqual(Product.objects.count(), 2)
 
@@ -136,13 +144,15 @@ country,sku,name,stock_change
     @parameterized.expand(
         [
             (
-                """
-country,sku,name,stock_change
-"dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
-"gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
-"ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
-"ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
-                """,
+                textwrap.dedent(
+                    """
+                        country,sku,name,stock_change
+                        "dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
+                        "gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
+                        "ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
+                        "ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
+                    """
+                ),
                 1,
                 4,
                 "e920c573f128",
@@ -150,14 +160,16 @@ country,sku,name,stock_change
                 58,
             ),
             (
-                """
-country,sku,name,stock_change
-"dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
-"gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
-"ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
-"ke","e920c573f128","Ramirez-Molina Granite Pizza","-5"
-"ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
-                """,
+                textwrap.dedent(
+                    """
+                        country,sku,name,stock_change
+                        "dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
+                        "gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
+                        "ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
+                        "ke","e920c573f128","Ramirez-Molina Granite Pizza","-5"
+                        "ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
+                    """
+                ),
                 1,
                 4,
                 "e920c573f128",
@@ -165,14 +177,16 @@ country,sku,name,stock_change
                 53,  # 58 - 5 == 53
             ),
             (
-                """
-country,sku,name,stock_change
-"dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
-"gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
-"ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
-"ke","e920c573f129","Ramirez-Molina Granite Pizza hut","5"
-"ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
-                """,
+                textwrap.dedent(
+                    """
+                        country,sku,name,stock_change
+                        "dz","e920c573f128","Ramirez-Molina Granite Pizza","32"
+                        "gh","e920c573f128","Ramirez-Molina Granite Pizza","51"
+                        "ke","e920c573f128","Ramirez-Molina Granite Pizza","58"
+                        "ke","e920c573f129","Ramirez-Molina Granite Pizza hut","5"
+                        "ug","e920c573f128","Ramirez-Molina Granite Pizza","63"
+                    """
+                ),
                 2,
                 5,
                 "e920c573f129",
